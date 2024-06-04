48fd7443337aa42af6ca7238cb511cc3a819dbc2
Abner <1228428816@qq.com>
2024-06-05 01:25:20 +0800

    chore: Update button and dropdown styles for better user experience

---
+++ js/dropdown.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+    var dropdowns = ['dropdown2', 'dropdown3', 'dropdown4'];
+
+    dropdowns.forEach(function(dropdownId) {
+        var bt = document.getElementById('bt' + dropdownId.slice(-1));
+        var dropdown = document.getElementById(dropdownId);
+
+        bt.addEventListener('mouseover', function() {
+            dropdown.style.display = 'block';
+        });
+
+        dropdown.addEventListener('mouseover', function() {
+            dropdown.style.display = 'block';
+        });
+
+        bt.addEventListener('mouseout', function() {
+            dropdown.style.display = 'none';
+        });
+
+        dropdown.addEventListener('mouseout', function() {
+            dropdown.style.display = 'none';
+        });
+    });
+}
